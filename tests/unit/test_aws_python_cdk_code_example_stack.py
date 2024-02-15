f541878c397b907c8b53d65e5c13d83465c7f1eb
Lint Action <lint-action@samuelmeuli.com>
2024-02-15 19:32:16 +0000

    Fix code style issues with Black

--- tests/unit/test_aws_python_cdk_code_example_stack.py
+++ tests/unit/test_aws_python_cdk_code_example_stack.py
@@ -1,7 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from aws_python_cdk_code_example.aws_python_cdk_code_example_stack import AwsPythonCdkCodeExampleStack
+from aws_python_cdk_code_example.aws_python_cdk_code_example_stack import (
+    AwsPythonCdkCodeExampleStack,
+)
+
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in aws_python_cdk_code_example/aws_python_cdk_code_example_stack.py
@@ -9,6 +12,7 @@ def test_sqs_queue_created():
     app = core.App()
     stack = AwsPythonCdkCodeExampleStack(app, "aws-python-cdk-code-example")
     template = assertions.Template.from_stack(stack)
+
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
 #         "VisibilityTimeout": 300
